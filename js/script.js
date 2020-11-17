39f6034c5bed8289cf3665d94ad013f161c88b4d
MarcoB <mrc.bsllt@gmail.com>
2020-11-17 14:16:37 +0100

    aggiunto il bottone

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-// stampo i numeri da 1 a 100
+var button = document.getElementById("button");
 var numbersList = document.getElementById("numbers_list");
 var j;
-for (var i = 1; i < 101; i++) {
 
-  if((i % 3 == 0) && (i % 5 == 0)) {
-    j = "<li class='green'>" + "FizzBuzz" + "</li>";
-  } else if (i % 3 == 0) {
-    j = "<li class='red'>" + "Fizz" + "</li>";
-  } else if (i % 5 == 0) {
-    j = "<li class='blue'>" + "Buzz" + "</li>";
-  } else {
-    j = i;
+
+button.addEventListener("click",
+  function () {
+    for (var i = 1; i < 101; i++) {
+
+      if((i % 3 == 0) && (i % 5 == 0)) {
+        j = "<li class='green'>" + "FizzBuzz" + "</li>";
+      } else if (i % 3 == 0) {
+        j = "<li class='red'>" + "Fizz" + "</li>";
+      } else if (i % 5 == 0) {
+        j = "<li class='blue'>" + "Buzz" + "</li>";
+      } else {
+        j = i;
+      }
+
+      numbersList.innerHTML += "<li>" + j + "</li>";
+    }
   }
-
-
-  numbersList.innerHTML += "<li>" + j + "</li>";
-}
+);
