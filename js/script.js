67d128fd6d66dfc4eb90a2632bb39e7f047a8135
MarcoB <mrc.bsllt@gmail.com>
2020-11-17 14:10:55 +0100

    aggiunto un po' di css

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ var j;
 for (var i = 1; i < 101; i++) {
 
   if((i % 3 == 0) && (i % 5 == 0)) {
-    j = "FizzBuzz";
+    j = "<li class='green'>" + "FizzBuzz" + "</li>";
   } else if (i % 3 == 0) {
-    j = "Fizz";
+    j = "<li class='red'>" + "Fizz" + "</li>";
   } else if (i % 5 == 0) {
-    j = "Buzz";
+    j = "<li class='blue'>" + "Buzz" + "</li>";
   } else {
     j = i;
   }
